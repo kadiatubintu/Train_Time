3c617c30b92d7bae48d22e30910057c7039c1069
Kadiatu <kadibaf@yahoo.com>
2017-07-09 01:37:58 -0400

    2nd commit
    - Added the moment.js library to calculate the time
    - debugged the firebase empty strings.

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyA6uVNrT20MAettMT_oicemAF3fUDTVVzo",
-    authDomain: "train-schedule-808fb.firebaseapp.com",
-    databaseURL: "https://train-schedule-808fb.firebaseio.com",
-    projectId: "train-schedule-808fb",
-    storageBucket: "",
-    messagingSenderId: "1059319356598"
-  };
-  firebase.initializeApp(config);
+   apiKey: "AIzaSyA6uVNrT20MAettMT_oicemAF3fUDTVVzo",
+   authDomain: "train-schedule-808fb.firebaseapp.com",
+   databaseURL: "https://train-schedule-808fb.firebaseio.com",
+   projectId: "train-schedule-808fb",
+   storageBucket: "train-schedule-808fb.appspot.com",
+   messagingSenderId: "1059319356598"
+ };
+ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$("#add-train-btn").on("click", function(event){
+$("form").on("submit", function(event){
   event.preventDefault();
-});
 
 var trainName = $("#name-input").val().trim();
 var trainDestination = $("#dest-input").val().trim();
-var trainTime = $("#time-input").val().trim();
-var trainFrequency = $("#freq-input").val().trim();
+// var trainTime = $("#time-input").val().trim();
+ var trainFrequency = $("#time-input").val().trim();
+// var trainFrequency = $("#freq-input").val().trim();
+var trainTime = $("#freq-input").val().trim();
+
 
 var newTrain = {
   name: trainName,
@@ -29,6 +31,8 @@ var newTrain = {
 };
 
 database.ref().push(newTrain);
+ console.log(newTrain)
+});
 
 $("#name-input").val("");
 $("#dest-input").val("");
@@ -39,10 +43,13 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey){
 
   var trainName = childSnapshot.val().name;
   var trainDestination = childSnapshot.val().dest;
-  var trainTime = childSnapshot.val().time;
   var trainFrequency = childSnapshot.val().frequency;
+  var trainTime = childSnapshot.val().time;
 
-  $("#train-table > tbody").append("<tr><td>" + name + "</td><td>" + dest + "</td><td>" +
-  time + "</td><td>" + frequency +  "</td></tr>");
+  var trainArrival = /*moment().format('LT') - trainFrequency;*/ moment().startOf('hour').fromNow('12:06');
+
+
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
+  trainTime + "</td><td>" + trainFrequency + "</td><td>" + trainArrival + " away" +  "</td></tr>");
 
 });
